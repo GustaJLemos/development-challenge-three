c8facb81f42adefa20c55deff8ade888735e6216
GustaJLemos <gustavolemosmendes@gmail.com>
2022-08-18 00:51:24 -0300

    Fix: Alterando tamanho das fontes, e consumindo type de Margin

--- and-now/src/components/molecules/Typography/index.tsx
+++ and-now/src/components/molecules/Typography/index.tsx
@@ -1,11 +1,12 @@
 import { ReactNode } from 'react';
 import { Text, TextProps, View } from 'react-native';
 import { COLORS } from '../../../global/styles';
+import { Margin } from '../../../types/Margin';
 import { responsiveSize } from '../../../utils/responsiveSize';
 
 import { styles } from './styles';
 
-type Props = TextProps & {
+type Props = TextProps & Margin & {
   children: ReactNode;
   textType?: 'title' | 'subtitle' | 'text' | 'small';
   textColor?: string;
@@ -13,30 +14,35 @@ type Props = TextProps & {
 
 const TYPOGRAPHY_TYPES = {
   title: {
-    fontSize: responsiveSize(2.4),
+    fontSize: responsiveSize(2),
     fontFamily: 'Poppins_700Bold',
   },
   subtitle: {
-    fontSize: responsiveSize(2),
+    fontSize: responsiveSize(1.8),
     fontFamily: 'Poppins_600SemiBold',
   },
   text: {
-    fontSize: responsiveSize(1.6),
+    fontSize: responsiveSize(1.4),
     fontFamily: 'Poppins_400Regular',
   },
   small: {
     fontSize: responsiveSize(1.2),
     fontFamily: 'Poppins_400Regular',
+    color: COLORS.grey10,
   },
 };
 
-export function Typography({ children, textType = 'text', textColor = COLORS.white, ...rest }: Props) {
+export function Typography({ children, textType = 'text', textColor = COLORS.white100, mt, mr, mb, ml, ...rest }: Props) {
   function calculateTextStyle() {
     return TYPOGRAPHY_TYPES[textType];
   }
 
   return (
-    <Text style={[styles.container, calculateTextStyle(), {color: textColor}, rest.style]}>
+    <Text style={[
+      styles.container, 
+      {color: textColor, marginTop: mt, marginRight: mr, marginBottom: mb, marginLeft: ml},
+      calculateTextStyle(), 
+      rest.style]}>
       {children}
     </Text> 
   );
