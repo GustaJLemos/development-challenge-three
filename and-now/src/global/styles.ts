ee2aa466b66dd82813bd57a5940ed761e28aa16c
GustaJLemos <gustavolemosmendes@gmail.com>
2022-08-18 00:52:08 -0300

    Feat: Adicionando mais algumas cores na aplicação

--- and-now/src/global/styles.ts
+++ and-now/src/global/styles.ts
@@ -1,8 +1,14 @@
 export const COLORS = {
+    blue1: '#577BDE',
     blue10: '#3453A8',
     blue100: '#202E55',
 
     salmon10: '#FF725E',
 
-    white: '#FFFFFF'
+    white10: '#F8F8F8',
+    white100: '#FFFFFF',
+
+    grey10: '#BDBDBD',
+
+    black: '#000000',
 }
