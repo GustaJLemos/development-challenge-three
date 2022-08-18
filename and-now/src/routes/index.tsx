62164075ccec6fb7a06eb37d3070d7783e5d783e
GustaJLemos <gustavolemosmendes@gmail.com>
2022-08-18 00:52:29 -0300

    Feat: Adicionando rotas na aplicação

---
+++ and-now/src/routes/index.tsx
@@ -0,0 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/Auth";
+import { COLORS } from "../global/styles";
+import { Home } from "../screens/Signed/Home";
+import { SignIn } from "../screens/SignIn";
+import { SignedRoutes } from "./signed.routes";
+
+export const AppRoutes = () => {
+  const { isLogged } = useContext(AuthContext);
+
+  return (
+    <NavigationContainer>
+      {isLogged ? <SignedRoutes /> : <SignIn />}
+    </NavigationContainer>
+  )
+}
---
+++ and-now/src/routes/signed.routes.tsx
@@ -0,0 +1,21 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { COLORS } from "../global/styles";
+import { Home } from "../screens/Signed/Home";
+import { SignIn } from "../screens/SignIn";
+
+const Stack = createNativeStackNavigator();
+
+export const SignedRoutes = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        contentStyle: {
+          backgroundColor: COLORS.white10
+        }
+      }}
+    >
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  )
+}
