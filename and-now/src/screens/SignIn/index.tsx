25a70e578454480c2b8452cbc0d74f3c6a5f36a6
GustaJLemos <gustavolemosmendes@gmail.com>
2022-08-22 01:04:07 -0300

    Feat: Mudando muitas coisas

--- and-now/src/screens/SignIn/index.tsx
+++ and-now/src/screens/SignIn/index.tsx
@@ -1,15 +1,16 @@
+import { useContext } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
-import { Text, TouchableOpacity, View } from 'react-native';
+
+import { AuthContext } from '../../contexts/Auth';
+
 import { BackgroundWrapper } from '../../components/atoms/BackgroundWrapper';
-import { COLORS } from '../../global/styles';
-import LandingBackgroundSvg from '../../assets/LandingBackground.svg';
-import { styles } from './styles';
 import { Typography } from '../../components/molecules/Typography';
 import { responsiveSize } from '../../utils/responsiveSize';
 import { Button } from '../../components/molecules/Button';
-import { useContext } from 'react';
-import { AuthContext } from '../../contexts/Auth';
-import { useNavigation } from '@react-navigation/native';
+
+import { COLORS } from '../../global/styles';
+import LandingBackgroundSvg from '../../assets/LandingBackground.svg';
 
 export function SignIn() {
   const { setIsLogged } = useContext(AuthContext);
@@ -20,17 +21,25 @@ export function SignIn() {
 
   return (
     <>
-      <StatusBar 
+      <StatusBar
         backgroundColor={COLORS.blue10}
         translucent
         hidden={true}
       />
       <BackgroundWrapper>
         <LandingBackgroundSvg />
-        <Typography textType='title' mt={responsiveSize(-8)} textColor={COLORS.salmon10} style={{fontSize: responsiveSize(3)}}>
+        <Typography
+          textType='title'
+          mt={responsiveSize(-8)}
+          textColor={COLORS.salmon10}
+          style={{fontSize: responsiveSize(3)}}
+        >
           E agora?
-        </Typography>  
-        <Typography mb={responsiveSize(1)} style={{fontSize: responsiveSize(2)}}>
+        </Typography>
+        <Typography
+          mb={responsiveSize(1)}
+          style={{fontSize: responsiveSize(2)}}
+        >
           O melhor jeito de {'\n'}
           descobrir receitas {'\n'}
           com os ingredientes {'\n'}
