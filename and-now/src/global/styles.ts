4aed0b703733cf1b648a210886898d97921efeb0
GustaJLemos <gustavolemosmendes@gmail.com>
2022-08-22 01:01:52 -0300

    Feat: Adicionando mais cores

--- and-now/src/global/styles.ts
+++ and-now/src/global/styles.ts
@@ -1,14 +1,18 @@
 export const COLORS = {
-    blue1: '#577BDE',
-    blue10: '#3453A8',
-    blue100: '#202E55',
+  blue1: '#577BDE',
+  blue10: '#3453A8',
+  blue100: '#202E55',
 
-    salmon10: '#FF725E',
+  salmon10: '#FF725E',
 
-    white10: '#F8F8F8',
-    white100: '#FFFFFF',
+  red10: '#E8290E',
 
-    grey10: '#BDBDBD',
+  white10: '#F8F8F8',
+  white100: '#FFFFFF',
 
-    black: '#000000',
+  grey1: '#D9D9D9',
+  grey10: '#BDBDBD',
+
+  black1: 'rgba(51, 51, 51, 86)',
+  black10: '#000000',
 }
